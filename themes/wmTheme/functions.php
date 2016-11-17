8e302d0908d6b4957fd683fb9c3ad6a3fd34f5a6
chris2hoyle <cthoyle@email.wm.edu>
2016-11-17 16:11:52 -0500

    added bootstrap + other css + js

--- themes/wmTheme/functions.php
+++ themes/wmTheme/functions.php
@@ -121,6 +121,9 @@ function entrepreneurship_scripts() {
 	if (is_page()){
 		wp_enqueue_style( 'page' , get_template_directory_uri() . '/css/page.css' );
 	}
+	wp_enqueue_style( 'animate', get_template_directory_uri() . '/css/animate.css' );
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6' );
+	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css',  array(), '4.5.0' );
 
 	wp_enqueue_style( 'navbar', get_template_directory_uri() . '/css/nav.css' );
 
@@ -130,6 +133,7 @@ function entrepreneurship_scripts() {
 
 	///////////////// scripts ///////////////////
 	wp_enqueue_script( 'entrepreneurship-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
 
 	wp_enqueue_script( 'entrepreneurship-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
