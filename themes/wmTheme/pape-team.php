4496b3d33618e4a87b8325ecbfebaa5bebc2e220
chris2hoyle <cthoyle@email.wm.edu>
2016-11-17 17:25:29 -0500

    added content to team

--- themes/wmTheme/pape-team.php
+++ themes/wmTheme/pape-team.php
@@ -7,23 +7,28 @@
 get_header(); ?>
 
 <div class="row">
-  <div class="col-sm-6 col-md-4">
-      <!-- <?php 
-        $args =  array( 
-          'post_type' => 'my-custom-post',
-          'orderby' => 'menu_order',
-          'order' => 'ASC'
-        );
-      $custom_query = new WP_Query( $args );
-            while ($custom_query->have_posts()) : $custom_query->the_post(); ?> -->
-
+  <div class="col-sm-5 col-md-3">
+   
       <div class="thumbnail">
 
-        <img src="..." alt="...">
-
+        <img src="<?php print(get_template_directory_uri()); ?>/img/henshaw.jpg" height="100" width="130" />
       <div class="caption">
         <h3>Graham Henshaw</h3>
-        <p>...</p>
+        <p>Executive Director Alan B. Miller Entrepreneurship Center</p>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="col-sm-5 col-md-3">
+   
+      <div class="thumbnail">
+
+        <img src="<?php print(get_template_directory_uri()); ?>/img/monark.jpeg" height="90" width="150" />
+      <div class="caption">
+        <h3>Ron Monark</h3>
+        <p>Managing Director, Alan B. Miller Entrepreneurship Center; Lecturer of Business</p>
       </div>
     </div>
   </div>
